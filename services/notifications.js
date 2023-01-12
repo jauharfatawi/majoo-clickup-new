6b113bfe0a28e7e331341eb27fb9969433ff0c65
Jauhar Fatawi <jauhar.fatawi@gmail.com>
2023-01-12 23:14:13 +0700

    tambahkan timeout ke gchat & minor lain

--- services/notifications.js
+++ services/notifications.js
@@ -3,13 +3,14 @@ const moment = require('moment')
 const axios = require('axios')
 
 const gChatDeployment = async (data) => {
-    let text = `🔔 New deployment submitted at ${moment().format('YYYY-DD-MM hh:mm:ss')} with details: \n>- Deployment Name: ${data.name} \n>- Squad: ${data.squad.toUpperCase()} \n>- Estimated Deployment: ${moment(data.date.split(' ').join('+')).format('YYYY-DD-MM hh:mm:ss')} \n>- Deployment ETA: ${data.eta} hour(s)\n>- Downtime: ${data.downtime} \n>- PRD/HLA: ${data.docs} \n>- Impacted App Services: ${data.service}`
+    let text = `New deployment submitted at ${moment().format('YYYY-DD-MM hh:mm:ss')} with details: \n>- Deployment Name: ${data.name} \n>- Squad: ${data.squad.toUpperCase()} \n>- Estimated Deployment: ${moment(data.date.split(' ').join('+')).format('YYYY-DD-MM hh:mm:ss')} \n>- Deployment ETA: ${data.eta} hour(s)\n>- Downtime: ${data.downtime} \n>- PRD/HLA: ${data.docs} \n>- Impacted App Services: ${data.service}`
     axios({
         method: "POST",
         url: config.webhook.gchatDeployment,
         data: {
             text: text
-        }
+        },
+        timeout: 60000
     });
 
     return 'OK'
