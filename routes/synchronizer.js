c2bf08fd38c7ecdc54c9755a4f4a7df912a1a49a
Satrio Wibowo <satrio.bws@gmail.com>
2023-08-02 16:43:09 +0700

    menambahkan fungsi counter_feedback

--- routes/synchronizer.js
+++ routes/synchronizer.js
@@ -20,4 +20,13 @@ router.post('/ideation', async function (req, res, _next) {
     }
 });
 
+router.post('/counter_feedback', async function (req, res, _next) {
+    try {
+        res.json(await synchronizer.counterFeedback(req.body.payload, req.query.type));
+    } catch (err) {
+        console.error(err.message);
+        res.status(err.statusCode || 500).json({ 'message': err.message });
+    }
+});
+
 module.exports = router;
