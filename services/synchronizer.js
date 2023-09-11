9ea85c949bd1fd85cd4095ed8668bb98a71aef96
Satrio Wibowo <satrio.bws@gmail.com>
2023-09-11 12:11:15 +0700

    Update synchronizer.js

--- services/synchronizer.js
+++ services/synchronizer.js
@@ -267,7 +267,7 @@ async function subtaskSync(payload) {
             }
         
             if (parent_reviewer) {
-                reviewer = parent_reviewer
+                reviewer = parent_reviewer[0].id
 
                 await axios({
                     method: "POST",
@@ -279,7 +279,7 @@ async function subtaskSync(payload) {
             }
 
             if (parent_pm) {
-                pm = parent_pm
+                pm = parent_pm[0].id
                 
                 await axios({
                     method: "POST",
