e44b5de1b35429d6c6184d5f9eadf28d0a317bc6
Satrio Wibowo <satrio.bws@gmail.com>
2023-09-19 04:41:27 +0700

    Update synchronizer.js

--- services/synchronizer.js
+++ services/synchronizer.js
@@ -239,9 +239,14 @@ async function subtaskSync(payload) {
         let pm = task.custom_fields[4].value ? task.custom_fields[4].value : false;
         
         
-        console.log(epic_release)
-        console.log(reviewer[0])
-        console.log(pm[0])
+        console.log(task.custom_fields[0])
+        console.log(task.custom_fields[3])
+        console.log(task.custom_fields[4])
+
+        
+        console.log(task.custom_fields[8])
+        console.log(task.custom_fields[16])
+        console.log(task.custom_fields[17])
         
         
         let pointer = (task.parent) ? task.parent : false;
