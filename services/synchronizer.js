76de67a7adb280ee6c692bac555849d9bdfb3f2a
Satrio Wibowo <satrio.bws@gmail.com>
2023-09-21 11:40:25 +0700

    satrio - debug automation #3

--- services/synchronizer.js
+++ services/synchronizer.js
@@ -268,32 +268,32 @@ async function subtaskSync(payload) {
             return i.id == pm_cf_id;
         });
         console.log(pm[0].value);
-        // if (typeof pm[0].value !== 'undefined' && pm[0].value) {
-        //     console.log('YOOY');
-        //     await axios({
-        //         method: "POST",
-        //         url: `https://api.clickup.com/api/v2/task/${task.id}/field/${pm_cf_id}`,
-        //         data: {
-        //             "value": {add: [pm[0].value]}
-        //         }
-        //     });
-        // }
+        if (typeof pm[0].value !== 'undefined' && pm[0].value) {
+            console.log('YOOY');
+            await axios({
+                method: "POST",
+                url: `https://api.clickup.com/api/v2/task/${task.id}/field/${pm_cf_id}`,
+                data: {
+                    "value": {add: [pm[0].value[0].id]}
+                }
+            });
+        }
 
-        //  // Set reviewer from parent task
-        // let reviewer = await asyncFilter(parent.custom_fields, async (i) => {
-        //     return i.id == reviewer_cf_id;
-        // });
-        // console.log(reviewer[0].value);
-        // if (typeof reviewer[0].value !== 'undefined' && reviewer[0].value) {
-        //     console.log('YUUY');
-        //     await axios({
-        //         method: "POST",
-        //         url: `https://api.clickup.com/api/v2/task/${task.id}/field/${reviewer_cf_id}`,
-        //         data: {
-        //             "value": {add: [reviewer[0].value]}
-        //         }
-        //     });
-        // }
+         // Set reviewer from parent task
+        let reviewer = await asyncFilter(parent.custom_fields, async (i) => {
+            return i.id == reviewer_cf_id;
+        });
+        console.log(reviewer[0].value);
+        if (typeof reviewer[0].value !== 'undefined' && reviewer[0].value) {
+            console.log('YUUY');
+            await axios({
+                method: "POST",
+                url: `https://api.clickup.com/api/v2/task/${task.id}/field/${reviewer_cf_id}`,
+                data: {
+                    "value": {add: [reviewer[0].value[0].id]}
+                }
+            });
+        }
 
         
         //  // Set quarter from parent task
