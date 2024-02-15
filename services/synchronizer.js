e83d8e9dd9d7cd1024b7cd7666dd7e5b5f10969b
iamsatrio <satrio.bws@gmail.com>
2024-02-15 17:28:00 +0700

    add auto complete status product feedback

--- services/synchronizer.js
+++ services/synchronizer.js
@@ -357,7 +357,7 @@ async function autoCompleteStatus(payload) {
         let task = payload
         
         let task_status = task.status.status || false;
-        let dependency_card = task.dependencies[0] || false;
+        let dependency_card = task.dependencies || false;
 
         console.log(task_status)
         console.log(dependency_card)
